c89bec88b7e377c387ae84d9c913cbf68af40fc5
anthoni <anthoni.nelli@pm.me>
2024-03-20 12:41:21 +0000

    add git info into the actuator info endpoint

--- start.sh
+++ start.sh
@@ -1,10 +1,11 @@
-# export JAVA_TOOL_OPTIONS="-javaagent:opentelemetry-javaagent.jar"
 #export OTEL_TRACES_EXPORTER=none 
 #export OTEL_METRICS_EXPORTER=logging
 #export OTEL_LOGS_EXPORTER=logging
-export OTEL_SERVICE_NAME="course-tracker"
+# export OTEL_SERVICE_NAME="course-tracker"
 export OTEL_EXPORTER_OTLP_ENDPOINT=http://localhost:4317
 export OTEL_METRIC_EXPORT_INTERVAL=15000 
+export OTEL_PROPAGATORS="tracecontext"
+# export OTEL_RESOURCE_ATTRIBUTES="environment=dev"
 java -jar target/course-tracker-app-jar-1.0.0.jar
 
 
