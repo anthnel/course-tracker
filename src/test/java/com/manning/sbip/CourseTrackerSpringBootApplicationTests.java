1cd9d6fd08dc08b91f7dc20839023e4d72ca964e
anthoni <anthnel@proton.me>
2024-04-03 07:58:56 +0000

    add integration test

--- src/test/java/com/manning/sbip/CourseTrackerSpringBootApplicationTests.java
+++ src/test/java/com/manning/sbip/CourseTrackerSpringBootApplicationTests.java
@@ -1,18 +1,39 @@
 package com.manning.sbip;
 
+import static org.junit.Assert.assertThat;
+
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.reactive.server.WebTestClient;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import com.manning.sbip.model.Course;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("integration")
 @Testcontainers
 public class CourseTrackerSpringBootApplicationTests {
 
+    @Autowired
+    private WebTestClient webTestClient;
+
     @Test
-    public void contextLoads(){
+    void whenPostRequestThenCourseCreated() {
+
+        var course1 = new Course(null, "Rapid Spring Boot Application Development", "Spring", 4,
+                "Learn Enterprise Application Development with Spring Boot");
+
+        webTestClient
+                .post()
+                .uri("/addcourse")
+                .bodyValue(course1)
+                .exchange()
+                .expectStatus().isOk();
 
     }
-    
+
 }
